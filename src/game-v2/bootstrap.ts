21b321e0216396c744c921584348f737c676c7ce
lienowen <lienowen@outlook.com>
2026-07-18 15:21:40 +0800

    fix(input): ignore invalid false interactive hit areas

--- src/game-v2/bootstrap.ts
+++ src/game-v2/bootstrap.ts
@@ -3,10 +3,31 @@ import { crazyGamesPlatform } from "../platform/crazyGamesPlatform";
 import { DAY_ONE_CONTENT } from "./content/day01";
 import { ImmersiveDayOneScene } from "./presentation/ImmersiveDayOneScene";
 
+type GuardedGameObjectPrototype = Phaser.GameObjects.GameObject & {
+  __safeInteractiveGuardInstalled?: boolean;
+};
+
+function installSafeInteractiveGuard(): void {
+  const prototype = Phaser.GameObjects.GameObject.prototype as GuardedGameObjectPrototype;
+  if (prototype.__safeInteractiveGuardInstalled) return;
+
+  const originalSetInteractive = prototype.setInteractive;
+  prototype.setInteractive = function safeSetInteractive(
+    hitArea?: Phaser.Types.Input.InputConfiguration | Phaser.Geom.Rectangle | Phaser.Geom.Circle | Phaser.Geom.Ellipse | Phaser.Geom.Polygon | false,
+    hitAreaCallback?: Phaser.Types.Input.HitAreaCallback,
+    dropZone?: boolean
+  ): Phaser.GameObjects.GameObject {
+    if (hitArea === false) return this;
+    return originalSetInteractive.call(this, hitArea, hitAreaCallback, dropZone);
+  };
+  prototype.__safeInteractiveGuardInstalled = true;
+}
+
 export async function bootstrapImmersiveGame(): Promise<Phaser.Game> {
   document.body.dataset.uiLanguage = "en";
   document.body.dataset.gameVersion = "architecture-v3";
 
+  installSafeInteractiveGuard();
   await crazyGamesPlatform.initialize();
   crazyGamesPlatform.loadingStart();
 
