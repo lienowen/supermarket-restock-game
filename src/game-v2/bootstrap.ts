b17d40f5719822d440d7a6af556170c04ff3c54e
lienowen <lienowen@outlook.com>
2026-07-18 11:34:21 +0800

    feat(v2): add clean game bootstrap

---
+++ src/game-v2/bootstrap.ts
@@ -0,0 +1,34 @@
+import Phaser from "phaser";
+import { crazyGamesPlatform } from "../platform/crazyGamesPlatform";
+import { DAY_ONE_CONTENT } from "./content/day01";
+import { ImmersiveDayOneScene } from "./presentation/ImmersiveDayOneScene";
+
+export async function bootstrapImmersiveGame(): Promise<Phaser.Game> {
+  await crazyGamesPlatform.initialize();
+  crazyGamesPlatform.loadingStart();
+
+  const game = new Phaser.Game({
+    type: Phaser.AUTO,
+    parent: "app",
+    width: DAY_ONE_CONTENT.world.width,
+    height: DAY_ONE_CONTENT.world.height,
+    backgroundColor: "#171712",
+    scale: {
+      mode: Phaser.Scale.FIT,
+      autoCenter: Phaser.Scale.CENTER_BOTH,
+      expandParent: true
+    },
+    render: {
+      antialias: true,
+      roundPixels: false,
+      pixelArt: false
+    },
+    input: {
+      activePointers: 3
+    },
+    scene: [ImmersiveDayOneScene]
+  });
+
+  crazyGamesPlatform.bindGame(game);
+  return game;
+}
