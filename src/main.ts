eeeb4f40402c4dd973d5a510e3c0de70e145261a
lienowen <lienowen@outlook.com>
2026-08-05 10:41:26 +0800

    Enable level-two restock polish

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import "./visual-comfort.css";
 import "./game/presentation/actors/installRestockCartCombo";
 import "./game/presentation/actors/installLevelOnePolish";
 import "./game/presentation/actors/installLevelOneWorkerMatteCleanup";
+import "./game/presentation/actors/installLevelTwoPolish";
 import "./game/presentation/actors/installLevelOneCompletionPolish";
 import { bootstrapGame } from "./game/bootstrap";
 
