7688fe7c319c8cd8ef4808dd586ed89bc168097c
lienowen <lienowen@outlook.com>
2026-08-10 09:26:55 +0800

    feat: enable Level 2 cart inventory visual

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import "./game/presentation/actors/installRestockCartCombo";
 import "./game/presentation/actors/installLevelOnePolish";
 import "./game/presentation/actors/installLevelOneWorkerMatteCleanup";
 import "./game/presentation/actors/installLevelTwoPolish";
+import "./game/presentation/actors/installLevelTwoCartInventoryVisual";
 import "./game/presentation/actors/installLevelTwoMemoryFeedback";
 import "./game/presentation/actors/installLevelOneCompletionPolish";
 import "./game/presentation/ui/installMatureRestockHud";
