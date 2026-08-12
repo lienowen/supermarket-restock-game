2491307e53654abc8a75d100a5ce6efb5bb1bae6
lienowen <lienowen@outlook.com>
2026-08-12 15:43:43 +0800

    Install Level 2 authored cooler layout

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import "./game/presentation/actors/installRestockCartCombo";
 import "./game/presentation/actors/installLevelOnePolish";
 import "./game/presentation/actors/installLevelOneWorkerMatteCleanup";
 import "./game/presentation/actors/installLevelTwoPolish";
+import "./game/presentation/actors/installLevelTwoCoolerLayout";
 import "./game/presentation/actors/installLevelTwoCartInventoryVisual";
 import "./game/presentation/actors/installLevelTwoMemoryFeedback";
 import "./game/presentation/actors/installLevelOneCompletionPolish";
